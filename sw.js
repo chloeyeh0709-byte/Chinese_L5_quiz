636920ff04c62aff6d55835e2e9c0ed1f16b47bc
chloeyeh0709-byte <chloeyeh0709@gmail.com>
2026-04-29 21:56:43 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'quiz-v1';
+const FILES = [
+  '/Chinese_L5_quiz/',
+  '/Chinese_L5_quiz/index.html',
+  '/Chinese_L5_quiz/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    })).catch(() => caches.match('/Chinese_L5_quiz/index.html'))
+  );
+});
